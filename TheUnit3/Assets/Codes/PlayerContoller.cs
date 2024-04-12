659ee6453d1ca163b555fb7e4d676ffd60434ae5
Taijohn Jones <145067431+TaijohnnBoi@users.noreply.github.com>
2024-04-12 08:39:40 -0700

    Part 4
    Almost there

--- TheUnit3/Assets/Codes/PlayerContoller.cs
+++ TheUnit3/Assets/Codes/PlayerContoller.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce;
     public float gravityModifier;
     public bool IsOnGround = true;
     public bool gameOver = false;
+  
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             IsOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -36,6 +40,7 @@ public class PlayerContoller : MonoBehaviour
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true); playerAnim.SetInteger("DeathType_int", 1);
         }
     } 
 }
